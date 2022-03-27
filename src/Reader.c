c5732c0ac73bf0e9a92818b7518c393b4e6caddc
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2022-03-27 12:18:23 -0400

    Implemented Pair struct, Reader_sort_count(), and Reader_reset_count(). Modified Reader_display_counters() so it uses Pairs instead

--- src/Reader.c
+++ src/Reader.c
@@ -7,8 +7,11 @@ Reader* Reader_init(const char* fn){
     Reader* r = (Reader*)malloc(sizeof(Reader));
     r->filename = fn;
     r->file_ptr = fopen(fn, "rb");
-    r->counters = (size_t*)malloc(0xFF * sizeof(size_t));
-    r->file_size = get_file_size(fn);
+    r->counters = (Pair*)calloc(0x100, sizeof(Pair));
+    r->file_size = _get_file_size(fn);
+    for(int i = 0; i < 0x100; i++){
+        r->counters[i].byte_value = bytes[i];
+    }
     return r;
 }
 
@@ -29,7 +32,7 @@ int Reader_count(Reader* r){
     if(r == NULL){
         return -1;
     }
-    char buffer[1];
+    unsigned char buffer[1];
     unsigned byte_index;
     for(int i = 0; i < r->file_size; i ++){
         fread(buffer, sizeof(char), 1, r->file_ptr);
@@ -37,8 +40,9 @@ int Reader_count(Reader* r){
         if(byte_index < 0 || byte_index >= 0xFF){
             return -1;
         }
-        r->counters[byte_index]++;
+        r->counters[byte_index].byte_count++;
     }
+    rewind(r->file_ptr);
     return 0;
 }
 
@@ -46,13 +50,38 @@ int Reader_display_counters(Reader* r){
     if(r == NULL){
         return -1;
     }
-    for(int i = 0; i < 0xFF; i++){
-        printf("%3x : %ld\n", i, r->counters[i]);
+    for(int i = 0; i < 0x100; i++){
+        printf("%2x : %ld\n", r->counters[i].byte_value, r->counters[i].byte_count);
     }
     return 0;
 }
 
-size_t get_file_size(const char* fn){
+int Reader_sort_count(Reader* r){
+    if(r == NULL){
+        return -1;
+    }
+    qsort(r->counters, 0x100, sizeof(Pair), _pair_compare);
+    for(int i = 0; i < 0x100; i++){
+        if(r->counters[i].byte_count != 0){
+            r->pairs_written++;
+        }
+    }
+    return 0;
+}
+
+int Reader_reset_count(Reader* r){
+    if(r == NULL){
+        return -1;
+    }
+    for(int i = 0; i < 0x100; i++){
+        r->counters[i].byte_value = bytes[i];
+        r->counters[i].byte_count = 0;
+    }
+    r->pairs_written = 0;
+    return 0;
+}
+
+size_t _get_file_size(const char* fn){
     FILE* f = fopen(fn, "rb");
     if(f == NULL){
         return -1;
@@ -63,3 +92,16 @@ size_t get_file_size(const char* fn){
     return ret;
 }
 
+int _pair_compare(const void* p1, const void* p2){
+    Pair* p1_ = (Pair*)p1;
+    Pair* p2_ = (Pair*)p2;
+    if(p1_->byte_count > p2_->byte_count){
+        return -1;
+    }
+    else if(p1_->byte_count < p2_->byte_count){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
