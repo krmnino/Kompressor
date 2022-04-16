da8a6ef2ee142822294c25f1f8ed82d4f900775e
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2022-04-16 11:26:08 -0400

    code clean up

--- src/Reader.c
+++ src/Reader.c
@@ -7,11 +7,17 @@ Pair* Pair_init(unsigned short bv, size_t bc){
     Pair* p = (Pair*)malloc(sizeof(Pair));
     p->byte_count = bc;
     p->byte_value = bv;
+
     return p;
 }
 
 int Pair_free(Pair* p){
+    if(p == NULL){
+        return -1;
+    }
+
     free(p);
+
     return 0;
 }
 
@@ -19,6 +25,7 @@ Reader* Reader_init(const char* fn){
     if(fn == NULL){
         return NULL;
     }
+
     Reader* r = (Reader*)malloc(sizeof(Reader));
     r->filename = fn;
     r->file_ptr = fopen(fn, "rb");
@@ -28,6 +35,7 @@ Reader* Reader_init(const char* fn){
         r->counters[i].byte_value = (unsigned short)i;
         r->counters[i].byte_count = 0;
     }
+
     return r;
 }
 
@@ -35,10 +43,14 @@ int Reader_free(Reader* r){
     if(r == NULL){
         return -1;
     }
+
+    // Close file and check if successful
     int ret_code = fclose(r->file_ptr);
     if(ret_code != 0){
         return -1;
     }
+
+    // Free up counter array and Reader object
     free(r->counters);
     free(r);
     return 0;
@@ -96,6 +108,7 @@ int Reader_compress_count(Reader* r){
             r->pairs_written++;
         }
     }
+    
     return 0;
 }
 
@@ -125,6 +138,7 @@ int Reader_display_counters(Reader* r){
     if(r == NULL){
         return -1;
     }
+
     for(unsigned int i = 0; i < BLOCKS; i+=8){
         printf("%2x : %6d, "  , r->counters[i].byte_value    ,  r->counters[i].byte_count);
         printf("%2x : %6d, "  , r->counters[i + 1].byte_value,  r->counters[i + 1].byte_count);
@@ -133,8 +147,9 @@ int Reader_display_counters(Reader* r){
         printf("%2x : %6d, "  , r->counters[i + 4].byte_value,  r->counters[i + 4].byte_count);
         printf("%2x : %6d, "  , r->counters[i + 5].byte_value,  r->counters[i + 5].byte_count);
         printf("%2x : %6d, "  , r->counters[i + 6].byte_value,  r->counters[i + 6].byte_count);
-        printf("%2x : %6d\n", r->counters[i + 7].byte_value,  r->counters[i + 7].byte_count);
+        printf("%2x : %6d\n"  , r->counters[i + 7].byte_value,  r->counters[i + 7].byte_count);
     }
+
     return 0;
 }
 
@@ -142,20 +157,28 @@ int Reader_reset_count(Reader* r){
     if(r == NULL){
         return -1;
     }
+
     for(unsigned int i = 0; i < BLOCKS; i++){
         r->counters[i].byte_count = 0;
     }
+    
     r->pairs_written = 0;
     return 0;
 }
 
 size_t _get_file_size(const char* fn){
+    if(fn == NULL){
+        return NULL;
+    }
+
     FILE* f = fopen(fn, "rb");
     if(f == NULL){
         return -1;
     }
+
     fseek(f, 0, SEEK_END);
     long ret = ftell(f);
+
     fclose(f);
     return ret;
 }
@@ -163,13 +186,13 @@ size_t _get_file_size(const char* fn){
 int _pair_compare(const void* p1, const void* p2){
     Pair* p1_ = (Pair*)p1;
     Pair* p2_ = (Pair*)p2;
+    
     if(p1_->byte_count > p2_->byte_count){
         return -1;
     }
     else if(p1_->byte_count < p2_->byte_count){
         return 1;
     }
-    else{
-        return 0;
-    }
+    
+    return 0;
 }
