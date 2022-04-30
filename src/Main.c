0262537fbc7db7c4975fe3fa0a28b9a38df46a60
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2022-04-30 01:11:42 -0400

    implemented command line parameter input and validation

--- src/Main.c
+++ src/Main.c
@@ -1,10 +1,91 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 #include "Reader.h"
 #include "Huffman.h"
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
+    if(argc != 4 && argc != 5){
+        printf("USAGE: ./kompressor [INPUT_FILE] [-c/-d] [-ow] [OUTPUT_FILE]\n");
+        return -1;
+    }
+
+    int ret;
+
+    // Validate option flags
+    if(strcmp(argv[2], "-c") != 0 && strcmp(argv[2], "-d") != 0){
+        printf("ERROR: Invalid option. -c = compress. -d = decompress\n");
+        printf("USAGE: ./kompressor [INPUT_FILE] [-c/-d] [-ow] [OUTPUT_FILE]\n");
+        return -1;
+    }
+
+    // Check if input file exists
+    ret = access(argv[1], F_OK);
+    if(ret != 0){
+        printf("ERROR: Input file cannot be found.\n");
+        return -1;
+    }
+
+    // If program parameter count is 4
+    if(argc == 4){
+        // Check if output file exists
+        ret = access(argv[3], F_OK);
+        if(ret == 0){
+            printf("ERROR: Output file exists but overwrite option -ow is disabled.\n");
+            printf("USAGE: ./kompressor [INPUT_FILE] [-c/-d] [-ow] [OUTPUT_FILE]\n");
+            return -1;
+        }
+
+        // If -c option is passed, then compress
+        if(strcmp(argv[2], "-c") == 0){
+            Reader* r1 = Reader_init(argv[1]);
+            Reader_compress_count(r1);
+            Huffman* h1 = Huffman_init(r1, argv[3]);
+            Huffman_compress(h1, r1);
+            Huffman_free(h1);
+            Reader_free(r1);
+        }
+        // Else if -d option is passed, then decompress
+        else{
+            Reader* r2 = Reader_init(argv[1]);
+            Reader_decompress_count(r2);
+            Huffman* h2 = Huffman_init(r2, argv[3]);
+            Huffman_decompress(h2, r2);
+            Huffman_free(h2);
+            Reader_free(r2);
+        }
+    }
+    // Else if program parameter count is 5
+    else{
+        // Validate option flags
+        if(strcmp(argv[3], "-ow") != 0){
+            printf("ERROR: Invalid option. -ow = allow output file overwrite\n");
+            printf("USAGE: ./kompressor [INPUT_FILE] [-c/-d] [-ow] [OUTPUT_FILE]\n");
+            return -1;
+        }
+
+        // If -c option is passed, then compress
+        if(strcmp(argv[2], "-c") == 0){
+            Reader* r1 = Reader_init(argv[1]);
+            Reader_compress_count(r1);
+            Huffman* h1 = Huffman_init(r1, argv[4]);
+            Huffman_compress(h1, r1);
+            Huffman_free(h1);
+            Reader_free(r1);
+        }
+        // Else if -d option is passed, then decompress
+        else{
+            Reader* r2 = Reader_init(argv[1]);
+            Reader_decompress_count(r2);
+            Huffman* h2 = Huffman_init(r2, argv[4]);
+            Huffman_decompress(h2, r2);
+            Huffman_free(h2);
+            Reader_free(r2);
+        }
+    }
+/*
     const char* input_fn = "../tests/test_data1.txt";
     //const char* input_fn = "../tests/test_data2.cl";
     //const char* input_fn = "../tests/test_data3.txt";
@@ -17,20 +98,6 @@ int main(int argc, char* argv[]){
     const char* output_fn = "output.kom";
 
     const char* decom_fn = "decom.txt";
-
-    Reader* r1 = Reader_init(input_fn);
-    Reader_compress_count(r1);
-    Huffman* h1 = Huffman_init(r1, output_fn);
-    Huffman_compress(h1, r1);
-    Huffman_free(h1);
-    Reader_free(r1);
-
-    Reader* r2 = Reader_init(output_fn);
-    Reader_decompress_count(r2);
-    Huffman* h2 = Huffman_init(r2, decom_fn);
-    Huffman_decompress(h2, r2);
-    Huffman_free(h2);
-    Reader_free(r2);
-
+*/
     return 0;
 }
